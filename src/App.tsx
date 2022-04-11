5c21fb8656ee023c0a0676f0d4129861c86891ad
Sameer Gautam <sameer.gautam9860@gmail.com>
2022-04-11 22:26:05 +0545

    changed css trying to add TS

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,30 @@
-import React,{FC} from 'react';
+import React,{FC,useState,ChangeEvent, HTMLInputTypeAttribute} from 'react';
 import './App.css';
 
 const App:FC=() => {
+  const[text,setText] =useState<string>("");
+  const[days,setDays] =useState<number>();
+  const[todoList,setTodoList]=useState([]);
+  const handleOnChange =(e:ChangeEvent<HTMLInputElement>)=> {
+    if(e.target.name ==="items"){
+      setText(e.target.value)
+    }else {
+      setDays(Number(e.target.value))
+    }
+  }
   return (
     <div className="App">
       <div className="tophalf">
-        <input type="text" placeholder='type your reminder here ' />
-        <input type="text" placeholder='Deadline ' />
+        <div className='input-container'>
+        <input type="text" placeholder='type your reminder here ' name="items" onChange={handleOnChange} />
+        <input type="number" placeholder='Deadline ' name="days" onChange={handleOnChange}/>
+        </div>
+        <div className='button top'>
+        <button> Add task</button>
+        </div>
+      </div>
+      <div className='bottomhalf'>
+
       </div>
     </div>
   );
